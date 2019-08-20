569090ca872de74ca8fc076aa6d11c24579dbd14
Kelly Gothard <kellygothard@gmail.com>
2019-08-20 10:27:54 -0400

    mixitup download

--- storylabwebsite/share/views.py
+++ storylabwebsite/share/views.py
@@ -1,6 +1,12 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.template import loader
+
+
+from .models import Paper
 
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+    latest_paper_list = Paper.objects.order_by('-pub_date')[:5]
+    context = {'latest_paper_list': latest_paper_list}
+    return render(request, 'share/index.html', context)
